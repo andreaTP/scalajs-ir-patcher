dd37258bcea6e04a7190f2e345c7726bff49cbc6
Andrea <andrea.peruffo1982@gmail.com>
2015-09-17 08:14:42 +0200

    more understandable

--- main/library/src/main/scala/demo/Foo.scala
+++ main/library/src/main/scala/demo/Foo.scala
@@ -2,10 +2,10 @@ package demo.unicredit
 
 
 class Foo() {
-	//val hello = "hello"
-	var hello = "hello"
+	val hello = "hello"
+	/*var hello = "hello"
 
 	def setHello(value: String): Unit = {
 		hello = value
-	}
+	}*/
 }
